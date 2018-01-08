80196352ea16083349d90868d20746741a591a5e
Florian Cabot <florian.cabot@etu.unige.ch>
2018-01-08 13:53:20 +0100

    Fixed clang-format-report

--- dev/clang-format-report.sh
+++ dev/clang-format-report.sh
@@ -22,7 +22,9 @@ for i in "${@:2}"; do
 	result=$($1 $i -output-replacements-xml | grep -c "replacement ")
 	if [ "$result" != "0" ]; then
 		echo "File does not comply to clang-format :" $i
-		echo "$result"
+		$1 $i > foo
+		echo "$(diff $i foo)"
+		rm foo
 		exitcode=1
 	fi
 done
