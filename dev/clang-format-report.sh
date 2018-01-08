6427b5a49213b87b0fedbdbcdef1237dff2c9211
Florian Cabot <florian.cabot@etu.unige.ch>
2018-01-08 13:49:44 +0100

    Modified clang-format report to display what is going wrong

--- dev/clang-format-report.sh
+++ dev/clang-format-report.sh
@@ -22,6 +22,7 @@ for i in "${@:2}"; do
 	result=$($1 $i -output-replacements-xml | grep -c "replacement ")
 	if [ "$result" != "0" ]; then
 		echo "File does not comply to clang-format :" $i
+		echo "$result"
 		exitcode=1
 	fi
 done
